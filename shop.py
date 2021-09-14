bab0489fbf9efe203679f9da6dd6acca7c0dbeed
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-13 22:47:41 -0700

    Add files via upload

---
+++ shop.py
@@ -0,0 +1,8 @@
+l_vegShop= ["okra", "onion", "peas", "tomato", "corn", "ginger", "radish", "potato",
+            "carrot", "broccoli", "asparagus", "cauliflower", "cucumber", "eggplant", "green pepper"]
+print(l_vegShop)
+
+
+
+
+
